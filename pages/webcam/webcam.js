39d1cbce13c22054dd40ab6592d8845db9ba8302
mi-skam <40042054+mi-skam@users.noreply.github.com>
2024-01-30 10:01:06 +0000

    30. Jan. 2024, 11:01

---
+++ pages/webcam/webcam.js
@@ -0,0 +1,61 @@
+const nextWebcam = async () => {
+  const webcamEl = document.querySelector('#webcam');
+  const allVideoInputs = await getVideoInputs();
+  let nextDevice;
+
+  if (webcamEl.srcObject) {
+    const currentDeviceId = webcamEl.srcObject
+      .getVideoTracks()[0]
+      .getSettings().deviceId;
+    nextDevice = allVideoInputs.find(
+      videoInput => videoInput.deviceId !== currentDeviceId,
+    );
+  }
+
+  if (nextDevice) {
+    setWebcam(nextDevice);
+  }
+};
+
+const setWebcam = device => {
+  const webcamEl = document.querySelector('#webcam');
+  const constraints = { video: { deviceId: device.deviceId } };
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      webcamEl.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.error('Error switching camera:', error);
+    });
+};
+
+const setWebcamByLabel = async webcamLabel => {
+  // get all webcams
+  const detectedWebcams = await getVideoInputs();
+  // get webcam by label
+  const webcam = detectedWebcams.reduce((acc, cam) => {
+    acc = cam.find(cam => cam.label === webcamLabel);
+    return acc;
+  }, '');
+  if (webcam) {
+    setWebcam(webcam);
+  }
+};
+
+const getVideoInputs = async () => {
+  return navigator.mediaDevices
+    .enumerateDevices()
+    .then(function (devices) {
+      const videoDevices = devices.filter(
+        device => device.kind === 'videoinput',
+      );
+      return videoDevices;
+    })
+    .catch(function (error) {
+      console.error('Error enumerating devices:', error);
+    });
+};
+
+export { nextWebcam, getVideoInputs, setWebcam, setWebcamByLabel };
---
+++ pages/webcam/webcamSelect.js
@@ -0,0 +1,36 @@
+import GUI from 'lil-gui';
+import { nextWebcam, getVideoInputs } from './webcam';
+import './style.css';
+
+const state = {};
+
+document.querySelector('#app').innerHTML = `
+<p> Webcam demonstration </p>
+<button id="switchButton">Next Webcam</button>
+<video autoplay playsinline id="webcam"></video>
+`;
+
+const video = document.querySelector('#webcam');
+const switchButton = document.querySelector('#switchButton');
+
+const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
+
+if (hasGetUserMedia()) {
+  // enable button, if webcams are detected
+  state.allVideoInputs = await getVideoInputs();
+  switchButton.disabled = !(state.allVideoInputs.length > 1);
+
+  // switch webcams
+  switchButton.addEventListener('click', nextWebcam);
+
+  // enable webcam stream
+  const constraints = { video: true };
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    video.srcObject = stream;
+  });
+}
+
+const gui = new GUI();
+const webcam = gui.addFolder('Webcam');
+webcam.add(state, 'allVideoInputs');
+console.log(state);
