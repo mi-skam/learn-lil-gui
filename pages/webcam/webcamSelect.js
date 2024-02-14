4a6e20704e3731ef55227703cd80b4dbb9b31b38
mi-skam <40042054+mi-skam@users.noreply.github.com>
2024-02-14 21:10:56 +0000

    14. Feb. 2024, 22:10

--- pages/webcam/webcamSelect.js
+++ pages/webcam/webcamSelect.js
@@ -1,27 +1,20 @@
 import GUI from 'lil-gui';
-import Webcam from './webcam';
+import { Webcam } from '@new-objects/libs';
 import './style.css';
 
 document.querySelector('body').innerHTML = `
     <div id="app">
       <p>Webcam demonstration</p>
-      <button id="switchButton">Next Webcam</button>
       <video autoplay playsinline id="webcam"></video>
     </div>
 `;
 // init Webcam instance
-const webcam = new Webcam();
-await webcam.init();
-
-const switchButton = document.querySelector('#switchButton');
-
-switchButton.disabled = !(webcam.allWebcams.length > 1);
-switchButton.addEventListener('click', webcam.nextWebcam);
+const webcam = await Webcam.initialize();
 
 // gui
 const gui = new GUI();
 const config = {
-  current: webcam.currentWebcam.label,
+  current: webcam.settings.label,
   all: webcam.allWebcams.map(cam => cam.label),
 };
 
